1ba2d77fa533ae70678335ff2a35827715e75425
MUHAMMED IRFAN P <112098500+IrfanKpm@users.noreply.github.com>
2025-02-26 15:53:11 +0530

    fix : speaker issue

--- main.py
+++ main.py
@@ -5,17 +5,16 @@ from config import config
 def main():
     core.speaker.speak("Genisys is Starting")
     time.sleep(4)
-    core.speaker.speak("Please wait for a moment")
+    speaker.speak("Please wait for a moment")
     
     sleeping = False  # AI sleep mode flag
-    
     while True:
         state, command = core.listener.listen()
         
         if sleeping:
             print(f"[Sleeping] Received command: {command}")
             if command.lower() == "wake ai":
-                core.speaker.speak("Waking up")
+                speaker.speak("Waking up")
                 config.SLEEP_COUNT = 0  # Reset sleep count
                 sleeping = False
             continue  # Skip processing when sleeping
@@ -30,32 +29,32 @@ def main():
 
                 if cmd2 == "exit":
                     print(f"[~] output > Exiting")
-                    core.speaker.speak("Exiting")
+                    speaker.speak("Exiting")
                     break
 
                 elif "time" in cmd2:
                     ti = core.real_data.date_time_info()
                     print(f"[~] output > {ti}")
-                    core.speaker.speak(ti)
+                    speaker.speak(ti)
 
                 elif "weather" in cmd2:
                     location = cmd2.replace("weather ", "").strip()
                     result = core.real_data.get_weather(location=location)
                     print(f"[~] output > {result}")
-                    core.speaker.speak(result)
+                    speaker.speak(result)
 
             else:
                 print(f"[~] output > {result}")
-                core.speaker.speak(result)
+                speaker.speak(result)
 
         else:
             if config.SLEEP_COUNT < config.SLEEP_N:
                 print(command)
-                core.speaker.speak(command)
+                speaker.speak(command)
                 config.SLEEP_COUNT += 1
             
             if config.SLEEP_COUNT >= config.SLEEP_N:
-                core.speaker.speak("Entering sleep mode. Say 'wake ai' to continue.")
+                speaker.speak("Entering sleep mode. Say 'wake ai' to continue.")
                 print(f"[!] Entering sleep mode. Say 'wake ai' to continue.")
                 sleeping = True
 
@@ -64,4 +63,3 @@ if __name__ == "__main__":
         main()
     except KeyboardInterrupt:
         print("Program Terminated")
-
